eaf2502ea72f5f95fe3565770621bb5d5c15b5b8
Alex Nosov <alexnossofff@gmail.com>
2019-07-22 03:24:12 +0300

    Добавил конфиги

---
+++ match_veka/match_veka/config.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+import json
+import os
+
+cur_path = os.path.dirname(os.path.abspath(__file__))
+
+global_config_file = cur_path + "/configs/config.json"
+
+
+def load_config(filename):
+    configfile = open(filename, "r", encoding="utf-8")
+    jsonstring = configfile.read()
+    res = json.loads(jsonstring)
+    configfile.close()
+    return res
+
+
+params = load_config(global_config_file)
+secret = load_config(cur_path + "/" + params["config_secret_path"])
